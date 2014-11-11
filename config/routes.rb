15649ed963a7ac07d2a0d8677ae73697301f1ef7
Liz Cross <elizabeth.cross@gmail.com>
2014-11-10 20:57:45 -0800

    Updated registration page with name. Fixed display of app name in header.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  devise_for :customers
- #get 'static_pages/home'
- resources :products
+  devise_for :customers, :controllers => { registrations: 'registrations' }
+  #get 'static_pages/home'
+  resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
