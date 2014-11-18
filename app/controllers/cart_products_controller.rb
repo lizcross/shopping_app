9f33643373e0d0a2eeeb5951b94e6014fb39ea55
Liz Cross <elizabeth.cross@gmail.com>
2014-11-17 20:45:37 -0800

    Updated cart to update and delete items.

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,4 +1,6 @@
 class CartProductsController < ApplicationController
+	#need to add before_actions here
+	
 	def create
 		@cartProduct = current_customer.cart.cart_products.build(cart_product_params)
 	    if @cartProduct.save
@@ -9,9 +11,17 @@ class CartProductsController < ApplicationController
 	end
 
 	def update
+		@cartProduct = CartProduct.find(params[:id])
+		if @cartProduct.update_attributes(cart_product_params)
+			redirect_to cart_path
+		else
+			flash[:alert] = "The cart could not be updated at this time"
+		end
 	end
 
 	def destroy
+		CartProduct.find(params[:id]).destroy
+		redirect_to cart_path
 	end
 
 	private
